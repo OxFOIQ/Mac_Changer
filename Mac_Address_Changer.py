91c50c73ba1d09febde1b21f3ac4ef7d7585d1d1
Mzoughi Med Amine <76553571+MedAmyyne@users.noreply.github.com>
2023-12-30 22:26:14 +0100

    Add files via upload

---
+++ Mac_Address_Changer.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+import subprocess
+import optparse
+import pyfiglet
+def Change_Mac (interface,new_mac) :
+    print("[<_>]  Changing Mac Address for " + interface + " to "+ new_mac)
+    subprocess.call(["ifconfig " ,interface ," down"])
+    subprocess.call(["ifconfig " ,interface ,"hw" , "ether" ,new_mac] )
+    subprocess.call(["ifconfig" ,interface , "up"])
+
+def Get_Arguments():
+    parser =optparse.OptionParser()
+    parser.add_option("-i","-inter","--interface",dest="interface",help="Interface to change Mac Address")
+    parser.add_option("-m","--mac",dest="new_mac",help="The New Mac Address")
+    (options , arguments)= parser.parse_args()
+    if not options.interface :
+        print("<!> Please specify interface , use --help for more informations .")
+    elif not options.new_mac :
+        print("<!> Please specify new mac address , use --help for more informations .")
+    return options
+
+
+if __name__ == "__main__":
+    ascii_banner = pyfiglet.figlet_format("MaC'Hanger",font="bulbhead")
+    print(ascii_banner)
+    options = Get_Arguments()
+    Change_Mac(options.interface,options.new_mac)
+
+
