a8df5c778ce261dc629e21496322c1e1715b46cf
Mzoughi Med Amine <76553571+MedAmyyne@users.noreply.github.com>
2023-12-30 22:49:10 +0100

    Update Mac_Address_Changer.py

--- Mac_Address_Changer.py
+++ Mac_Address_Changer.py
@@ -2,25 +2,24 @@ import subprocess
 import optparse
 import pyfiglet
 
+def Change_Mac(interface, new_mac):
+    print("[<_>]  Changing Mac Address for " + interface + " to " + new_mac)
+    subprocess.run(["ifconfig", interface, "down"])
+    subprocess.run(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.run(["ifconfig", interface, "up"])
+
 def Get_Arguments():
-    parser =optparse.OptionParser()
-    parser.add_option("-i","--interface",dest="interface",help="Interface to change Mac Address")
-    parser.add_option("-m","--mac",dest="new_mac",help="The New Mac Address")
-    (options , arguments)= parser.parse_args()
-    if not options.interface :
-        print("<!> Please specify interface , use --help for more informations .")
-    elif not options.new_mac :
-        print("<!> Please specify new mac address , use --help for more informations .")
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change Mac Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="The New Mac Address")
+    options, _ = parser.parse_args()
+    if not options.interface or not options.new_mac:
+        parser.error("Please specify both interface and new MAC address. Use --help for more information.")
     return options
 
-
-def Change_Mac (interface,new_mac) :
-    print("[<_>]  Changing Mac Address for " + interface + " to "+ new_mac)
-    subprocess.call(["ifconfig " ,interface ," down"])
-    subprocess.call(["ifconfig " ,interface ,"hw" , "ether" ,new_mac] )
-    subprocess.call(["ifconfig" ,interface , "up"])
-
-ascii_banner = pyfiglet.figlet_format("MaC'Hanger")
-print(ascii_banner)
-options = Get_Arguments()
-Change_Mac(options.interface,options.new_mac)
+if __name__ == "__main__":
+    ascii_banner = pyfiglet.figlet_format("MaC'Hanger", font="bulbhead")
+    print(ascii_banner)
+    
+    options = Get_Arguments()
+    Change_Mac(options.interface, options.new_mac)
